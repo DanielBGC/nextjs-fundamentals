e3de76a1b02ce29846165e7277d4100954a368f8
DanielBGC <danielbgc02@gmail.com>
2023-03-19 15:52:18 -0300

    CSS modularizado

---
+++ src/pages/StyledPage.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/StyledPage.module.css';
+
+export default function styledPage() {
+  return(
+    <div className={styles.purpleDiv}>
+      <h1>Styled Page</h1>
+    </div>
+  )
+}
